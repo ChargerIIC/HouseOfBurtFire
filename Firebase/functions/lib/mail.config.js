970bf16a8ee1625530430d3a890eb8e4e9aef859
VBurt <ChargerIIC@yahoo.com>
2018-06-12 12:34:42 -0700

    Move of Firebase Function to its own repo
    Ionic's treeshaking kept including the firebase code

---
+++ Firebase/functions/lib/mail.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class MailConfig {
+}
+MailConfig.USERNAME = 'ChargerIIC@gmail.com';
+MailConfig.PASSWORD = 'SteelChargerGoogle1!';
+exports.MailConfig = MailConfig;
+//# sourceMappingURL=mail.config.js.map
---
+++ Firebase/functions/src/mail.config.ts
@@ -0,0 +1,4 @@
+export class MailConfig{
+    static USERNAME : string = 'ChargerIIC@gmail.com';
+    static PASSWORD : string = 'SteelChargerGoogle1!';
+}
